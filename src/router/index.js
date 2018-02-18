c293b2ca366cdd336387425aa97eba845fc8b7f8
Fukuda Naoto <never9612@gmail.com>
2018-02-19 04:37:09 +0900

    Update docs

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import GoalSelectPage from '@/components/GoalSelectPage'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
+  // mode: 'history',
   routes: [
     {
       path: '/',
