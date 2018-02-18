bb970cb5e9da91ebcb591ac9b93f24d66a87d918
Fukuda Naoto <never9612@gmail.com>
2018-02-19 04:34:46 +0900

    Update docs

--- config/index.js
+++ config/index.js
@@ -41,7 +41,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/walking-app',
 
     /**
      * Source Maps
